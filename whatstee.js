bc5c08accc71916d3c98d82cb22ec545038f6180
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-12-13 14:22:37 -0300

    feature: business rules

--- whatstee.js
+++ whatstee.js
@@ -1,25 +1,58 @@
-const button = `
-<rt-candidate-profile-information-editor-group-entry class="ng-star-inserted">
-    <a target="_blank" href="https://wa.me/" class="ng-star-inserted">
-        <img width="24" height="24" draggable="false" style="vertical-align: top;"
-            src="https://www.shareicon.net/data/512x512/2016/07/10/119959_whatsapp_512x512.png">
-    </a>
-</rt-candidate-profile-information-editor-group-entry>
-`
+/* eslint-disable no-unused-vars */
 
-let [email, phone, socials] = document.querySelectorAll('rt-candidate-profile-information-editor-group')
+let emails
+let phones
+let socials
 
-phone = [...phone.querySelectorAll('a')].map(a => a.innerText)
+const removeNotDigitCharacters = (phone) => '55' + phone.replace(/\D+/g, '')
+const addNinthDigit = (phone) => '9' + phone
+const addDDD = (phone, ddd) => ddd + phone
+const addCountryCode = (phone, code) => code + phone
 
-const insertWAButton = () => {
+const convertPhone = (phone) => {
+  phone = removeNotDigitCharacters(phone)
 
-    const buttons = document.querySelectorAll('rt-candidate-profile-information-editor-group')[2]
-        .querySelector('.field__values-list')
-        .innerHTML + button
+  const length = phone.length
 
-    document.querySelectorAll('rt-candidate-profile-information-editor-group')[2]
-        .querySelector('.field__values-list')
-        .innerHTML = buttons
+  if (length === 8) {
+    return addCountryCode(addDDD(addNinthDigit(phone), 47), 55)
+  } else if (length === 9) {
+    return addCountryCode(addDDD(phone, 47), 55)
+  } else if (length === 10) {
+    const splitedPhone = [phone.split('').slice(0, 2).join(''), phone.split('').slice(2).join('')]
+
+    return addCountryCode(addDDD(addNinthDigit(splitedPhone[1]), splitedPhone[0]), 55)
+  } else if (length === 11) {
+    return addCountryCode(phone, 55)
+  }
+
+  return phone
 }
 
-insertWAButton()
+const getWAButton = (phone) => `
+  <span class="ng-star-inserted" style="margin-top: 8px; margin-bottom: 8px; margin-right: 12px;">
+      <a target="_blank" href="https://wa.me/${phone}" class="ng-star-inserted">
+          <img width="24" height="24" draggable="false" style="vertical-align: top;"
+              src="https://www.shareicon.net/data/512x512/2016/07/10/119959_whatsapp_512x512.png">
+      </a>
+  </span>
+  `
+
+const insertWAButton = (phones) => {
+  const buttons = document.querySelectorAll('rt-candidate-profile-information-editor-group')[2]
+    .querySelector('.field__values-list')
+    .innerHTML + phones.map(getWAButton).join('')
+
+  document.querySelectorAll('rt-candidate-profile-information-editor-group')[2]
+    .querySelector('.field__values-list')
+    .innerHTML = buttons
+}
+
+// Execution code
+
+[emails, phones, socials] = document.querySelectorAll('rt-candidate-profile-information-editor-group')
+
+phones = [...phones.querySelectorAll('a')]
+  .map(({ innerText }) => convertPhone(innerText))
+
+insertWAButton(phones)
